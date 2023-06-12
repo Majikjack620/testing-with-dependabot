ee6320ac1e1ac8bf8a3db58cd2f35d7b6cd25afb
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-06-12 11:42:01 -0500

    Delete frontend directory

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-//TODO: Replace the xxxxx in the below url with the url created in API Gateway after deploying the backend via serverless.
-
-export const environment = {
-  state: 'production',
-  production: true,
-  apiUrl: 'https://cohuq9rsrb.execute-api.us-east-1.amazonaws.com/dev'
-}
